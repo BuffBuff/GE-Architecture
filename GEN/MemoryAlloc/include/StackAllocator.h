f09adb155cf5bf0926af07ced359cb30d2622d2f
Sebastian Larsson <sjereq@gmail.com>
2014-09-03 13:54:11 +0200

    Implement simple stack allocator.

--- GEN/MemoryAlloc/include/StackAllocator.h
+++ GEN/MemoryAlloc/include/StackAllocator.h
@@ -1,9 +1,11 @@
 #include <cstdint>
+#include <vector>
 
 namespace GENA
 {
 	class StackAllocator
 	{
+	public:
 		/**
 		 * Stack marker: Represents the current top of the
 		 * stack. You can only roll back to a marker, not to
@@ -38,5 +40,40 @@ namespace GENA
 		 * zero).
 		 */
 		void clear();
+
+	private:
+		std::vector<char> buffer;
 	};
+
+	inline StackAllocator::StackAllocator(uint32_t stackSizeBytes)
+	{
+		buffer.reserve(stackSizeBytes);
+	}
+
+	inline void* StackAllocator::alloc(uint32_t sizeBytes)
+	{
+		if (sizeBytes > buffer.capacity() - buffer.size())
+		{
+			throw std::exception("No more stack memory for you!");
+		}
+		auto prevEnd = buffer.end();
+		buffer.resize(buffer.size() + sizeBytes);
+
+		return &(*prevEnd);
+	}
+
+	inline StackAllocator::Marker StackAllocator::getMarker()
+	{
+		return buffer.size();
+	}
+
+	inline void StackAllocator::freeToMarker(Marker marker)
+	{
+		buffer.resize(marker);
+	}
+
+	inline void StackAllocator::clear()
+	{
+		buffer.clear();
+	}
 }
--- GEN/MemoryTest/Source/program.cpp
+++ GEN/MemoryTest/Source/program.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
 
 #include "MemoryAlloc.h"
+#include "StackAllocator.h"
 
 int main(int argc, char* argv[])
 {
 	std::cout << GENA::getMessage() << '\n';
+
+	GENA::StackAllocator stack(1024 * 1024);
+
+	for (int i = 0; i < 1024; ++i)
+	{
+		stack.alloc(1024);
+	}
+
+	stack.clear();
+
+	for (int i = 0; i < 1024; ++i)
+	{
+		stack.alloc(512);
+	}
+
+	GENA::StackAllocator::Marker marker = stack.getMarker();
+
+	for (int i = 0; i < 1024; ++i)
+	{
+		stack.alloc(512);
+	}
+
+	stack.freeToMarker(marker);
+
+	stack.alloc(512 * 1024);
+
+	try
+	{
+		stack.alloc(1);
+	}
+	catch (std::exception& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
 
 	return 0;
 };
